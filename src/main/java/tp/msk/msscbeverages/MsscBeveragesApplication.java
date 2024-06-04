f63a3a16da3745136695dd8e56b2c2f5b95047aa
Sane Generous <karim2k11@gmail.com>
2024-06-04 16:58:44 +0100

    Initial commit

---
+++ src/main/java/tp/msk/msscbeverages/MsscBeveragesApplication.java
@@ -0,0 +1,13 @@
+package tp.msk.msscbeverages;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsscBeveragesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsscBeveragesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/tp/msk/msscbeverages/MsscBeveragesApplicationTests.java
@@ -0,0 +1,13 @@
+package tp.msk.msscbeverages;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsscBeveragesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
